cdb3e27d3e8b37130f6139f6c92cf6097c2e739b
villinoid <vvvvvvos65@gmail.com>
2020-11-02 21:06:42 -0500

    Revert "done"
    This reverts commit 507f9064fdd8cc26bf3f4285a09a57601cf04efb.

--- material.c
+++ material.c
@@ -5,9 +5,7 @@
 #include "material.h"
 
 void print_material(struct material *m){
-	if (m){
-		printf("material: %s\tdensity: %d\n", m->name, m->density);
-	}
+	printf("material: %s\tdensity: %d\n", m->name, m->density);
 }
 
 struct material *new_material(char *n, int d) {
@@ -18,7 +16,6 @@ struct material *new_material(char *n, int d) {
 
   strncpy(nm->name, n, sizeof(nm->name)-1);
   nm->density = d;
-  nm->next= 0;
 
   return nm;
 }
@@ -26,45 +23,3 @@ struct material *new_material(char *n, int d) {
 void update_density(struct material *m, int nd){
 	m->density=nd;
 }
-
-void print_list(struct material *node){
-	if (node){
-		while(node->next){
-			print_material(node);
-			node=node->next;
-		}
-		print_material(node);
-	}
-}
-	;
-	
-struct material * insert_front(struct material *node,char *n, int d){
-	struct material*f=new_material(n,d);
-	f->next=node;
-	return f;
-}
-struct material * free_list(struct material *node){
-	while(node->next){
-		free(node);
-		node=node->next;
-	}
-	free(node);
-	return 0;
-}
-	
-struct material * remove_node(struct material *f, char *n, int d){
-	struct material *node=f;
-	if (d==node->density && !(strcmp(n,node->name))){
-		return f->next;
-	}
-	while(node->next){
-		if(d==node->next->density && !(strcmp(n,node->next->name))){
-			node->next=node->next->next;
-			return f;
-		}
-		node=node->next;
-	}
-	return f;
-}
-			
-	
--- material.h
+++ material.h
@@ -4,14 +4,10 @@
 struct material {
 	char name[50];
 	int density;
-	struct material *next;
 };
 
 void print_material(struct material*);
 struct material *new_material(char *, int);
 void update_density(struct material*, int);
-void print_list(struct material *);
-struct material * insert_front(struct material *,char *, int);
-struct material * free_list(struct material *);
-struct material * remove_node(struct material *, char*, int);
+
 #endif
